78db8a95532380bc28be24b4770fe28e6e923bf8
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-11 21:12:14 +0900

    Delete src directory

--- src/main/java/com/signition/samskybridge/upgrade/UpgradeService.java
+++
@@ -1,6 +0,0 @@
-
-package com.signition.samskybridge.upgrade;
-import org.bukkit.entity.Player;
-public class UpgradeService {
-    public void open(Player p){ p.sendMessage("업그레이드 GUI는 추후 제공됩니다."); }
-}
