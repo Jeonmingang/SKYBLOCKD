e7937fa8f2e517c4fecfa7c74551455686c2abfa
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-10-01 08:50:05 +0900

    Delete src directory

--- src/main/java/com/signition/samskybridge/util/Text.java
+++
@@ -1,8 +0,0 @@
-package com.signition.samskybridge.util;
-import org.bukkit.ChatColor;
-public final class Text {
-    private Text(){}
-    public static String color(String s){
-        return ChatColor.translateAlternateColorCodes('&', s == null ? "" : s);
-    }
-}
